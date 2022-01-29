16edef85bf60d8e7ba307d97bc275d6fc826d3c9
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-01-29 15:51:05 +0100

    Added test to PageComponent

---
+++ public/js/Components/PageComponent/PageComponent.test.js
@@ -0,0 +1,26 @@
+import PageComponent from "./PageComponent";
+
+describe("Given a PageComponent component", () => {
+  describe("When it's rendered", () => {
+    test("Then it should render a div", () => {
+      const container = document.createElement("div");
+
+      new PageComponent(container);
+
+      expect(container.querySelector("div")).not.toBeNull();
+    });
+  });
+
+  describe("When instantiated", () => {
+    test("Then it should display a paragraph inside a footer with 'Made By David Pegueroles' as it's content", () => {
+      const container = document.createElement("div");
+      const expectedTextContent = "Made By David Pegueroles";
+
+      new PageComponent(container);
+
+      expect(container.querySelector("div").innerHTML).toMatch(
+        expectedTextContent
+      );
+    });
+  });
+});
