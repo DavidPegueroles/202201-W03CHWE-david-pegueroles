c06c1b17c08ca94e06ca0a45a4ad6a31d3baba09
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-01-29 12:40:58 +0100

    Added NavigationComponent

---
+++ public/js/Components/NavigationComponent/NavigationComponent.js
@@ -0,0 +1,19 @@
+import Component from "../Component/Component";
+
+class NavogationComopnent extends Component {
+  constructor(parentElement, className) {
+    super(parentElement, className, "ul");
+
+    this.generateHTML();
+  }
+
+  generateHTML() {
+    this.element.innerHTML = `
+      <li><a href="index.html">Pokédex</a></li>
+      <li><a href="my-pokemon.html">My Pokémon</a></li>
+      <li><a href="pokeInfo.html">PokéInfo</a></li>
+    `;
+  }
+}
+
+export default NavogationComopnent;
