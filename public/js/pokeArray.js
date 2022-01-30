ff9fcbd8c29c518aec5b05a74b007117fedd7a96
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-01-30 12:34:10 +0100

    Added function to fetch to pokeApi

---
+++ public/js/pokeArray.js
@@ -0,0 +1,25 @@
+const firstPokemonIndex = 0;
+
+async function getNinePokemon() {
+  const fetchedArray = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=9&offset=${firstPokemonIndex}`
+  );
+
+  const pokemonArrayJson = await fetchedArray.json();
+
+  const pokemonObjects = pokemonArrayJson.results;
+
+  const pokemon = pokemonObjects.map(async (pokemonObject) => {
+    const fetchedPokemonObject = await fetch(pokemonObject.url);
+    const pokemonObjectJson = fetchedPokemonObject.json();
+
+    return pokemonObjectJson;
+  });
+
+  const singlePokemon = Promise.all(pokemon);
+  return singlePokemon;
+}
+
+const getPokemon = await getNinePokemon();
+
+export default getPokemon;
